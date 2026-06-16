0369a63bfa61a1b54f346324a29aea8b6552bc41
student <student@mail.ru>
2026-06-16 15:20:22 +0400

    ввод двух чисел и вывод их суммы

--- primer/Program.cs
+++ primer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, sum;
+            Console.Write("Введите первое число: ");
+            while (!double.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз:");
+            }
+            Console.Write("Введите второе число: ");
+            while (!double.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте еще раз:");
+            }
+            sum = num1 + num2;
+            Console.WriteLine($"Сумма: {num1} + {num2} = {sum}");
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
